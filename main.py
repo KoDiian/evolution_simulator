313d8d8d61bdf391b789237d6ff8727ae36c5198
KoDiian <tristan.bert.12@gmail.com>
2024-03-19 16:05:15 +0100

    Food.py

---
+++ main.py
@@ -0,0 +1,60 @@
+import pygame
+from food import Food
+import random
+
+pygame.init()
+
+# Constantes
+tilesize = 5 # Taille d'une tuile
+map = 32 # Multiplicateur de la taille du monde
+size = (20,20) # Taille du monde
+fps = 30 # fps du jeu
+
+# Couleurs
+color = {
+    "background_color": "#000000",
+}
+
+BLACK = (0, 0, 0)
+font = pygame.font.SysFont(None, 48)
+
+
+running = True
+screen = pygame.display.set_mode((size[0] * map, size[1] * map))
+clock = pygame.time.Clock()
+dt = 0
+
+screen.fill(color["background_color"])
+
+
+nourriture = Food()
+
+nbr_max_x = ((size[0] * map) / tilesize)
+nbr_max_y = ((size[1] * map) / tilesize)
+x = []
+y = []
+for i in range(int(nbr_max_x)):
+    x.append(int(i*tilesize))
+for i in range(int(nbr_max_y)):
+    y.append(int(i*tilesize))
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+
+
+
+    nourriture.draw(screen, tilesize, nbr_max_x, nbr_max_y, x, y)
+    
+
+
+    
+    pygame.display.flip()
+    dt = clock.tick(fps)
+pygame.quit()
